daaab72d78677701c9a32e0383931f4ac93af2b1
Student <student@MKS-LA-12.local>
2016-01-04 20:21:56 -0800

    MVP functional

--- js-docs/js-docs.js
+++ js-docs/js-docs.js
@@ -2,9 +2,18 @@ Docs = new Mongo.Collection("docs");
 
 if (Meteor.isClient) {
 
+  Meteor.subscribe('docs');
+
   Template.body.helpers({
     docs: function() {
-      return Docs.find({});
+      if (Session.get('showFavorited')) {
+        return Docs.find({checked: true}, {sort: {name: 1}});
+      } else {
+        return Docs.find({}, {sort: {name: 1}});
+      }
+    },
+    showFavorited: function() {
+      return Session.get('showFavorited');
     }
   });
 
@@ -16,23 +25,55 @@ if (Meteor.isClient) {
       var url = event.target.url.value;
       var src = event.target.src.value;
 
-      Docs.insert({
-        name: name,
-        url: url,
-        src: src,
-        createdAt: new Date()
-      });
+      Meteor.call('addDoc', name, url, src);
 
       event.target.name.value = "";
       event.target.url.value = "";
       event.target.src.value = "";
+    },
+    "change .show-favorited input": function(event) {
+      Session.set('showFavorited', event.target.checked);
     }
   });
 
+  Template.doc.events({
+    "click .toggle-checked": function() {
+      Meteor.call('favorite', this._id, ! this.checked);
+    },
+    "click .delete": function() {
+      Meteor.call('delete', this._id);
+    }
+  });
+
+  Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_ONLY'
+  });
 }
 
+Meteor.methods({
+  addDoc: function(name, url, src) {
+    if (! Meteor.userId()) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    Docs.insert({
+      name: name,
+      url: url,
+      src: src,
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  favorite: function(docId, favorite) {
+    Docs.update(docId, { $set: {checked: favorite} });
+  },
+  delete: function(docId) {
+    Docs.remove(docId);
+  }
+})
+
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  Meteor.publish('docs', function() {
+    return Docs.find({owner: this.userId});
   });
 }
